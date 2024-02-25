c1242e693965c6ed557dbf48c7b85b2278e30037
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-25 10:58:29 +0530

    prisma orm initialised for postgres db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,43 @@
 import { Hono } from 'hono'
+const app = new Hono().basePath('/api/v1')
 
-const app = new Hono()
+// routing
+app.get('/', c => { return c.json({ message: 'Hello, World!' }) })
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/user/signup', (c) => {
+  return c.json({
+    message: 'User signup'
+  })
 })
+
+app.post('/user/signin', (c) => {
+  return c.json({
+    message: 'User signin'
+  })
+})
+
+app.post('/blog', (c) => {
+  return c.json({
+    message: 'Create blog'
+  })
+})
+
+app.put('/blog', (c) => {
+  return c.json({
+    message: 'Update blog'
+  })
+})
+
+app.get('/blog/:id', (c) => {
+  return c.json({
+    message: 'Get blog: ' + c.req.param('id')
+  })
+})
+
+// any wrong route
+app.get('*', (c) => {
+  return c.json({ message: 'this may not be what you are finding' }, 404)
+})
+
 
 export default app
